c0be6d16599c2cf69816ec59f7964bead31f66ec
Ryan Seal <ryan@code.splrk.net>
2017-07-13 14:05:57 +0200

    Added realmd to prereqs

--- installprereqs.sh
+++ installprereqs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt install -y krb5-user samba sssd ntp
+apt install -y krb5-user samba sssd ntp realmd adcli libwbclient-sssd sssd-tools samba-common packagekit samba-common-bin samba-libs ntpstat
 
 export DEFAULTREALM=$(awk -f krb5defaultrealm.awk /etc/krb5.conf)
 
